7b9a47ece46d9e022551903b9d3f94778b252a8f
Aryansh Rao <aryanshrao8744@gmail.com>
2024-12-23 21:01:28 +0530

    React fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import GridLayout from "@/app/components/GridLayout";
+import React from 'react';
+
 
 export default function Home() {
   return (
     <div>
-      
       <GridLayout />
     </div>
   );
